120c734c7180c56c491dc11790c52ab8159ba722
DrHuangMHT <DrHuangMHT@outlook.com>
2022-10-17 20:04:51 +0800

    add contact icon

---
+++ src/components/ContactIcon.vue
@@ -0,0 +1,35 @@
+<script setup>
+import pixiv from "../assets/icons/pixiv.ico";
+import bcy from "../assets/icons/bcy.svg";
+import bilibili from "../assets/icons/bilibili.svg";
+import lofter from "../assets/icons/lofter.svg";
+import twitter from "../assets/icons/twitter.svg";
+import weibo from "../assets/icons/weibo.svg";
+const props = defineProps({
+  type: String,
+  href: String,
+});
+let source = "";
+switch (props.type) {
+  case "bcy":
+    source = bcy;
+    break;
+  case "bilibili":
+    source = bilibili;
+    break;
+  case "lofter":
+    source = lofter;
+    break;
+  case "pixiv":
+    source = pixiv;
+    break;
+  case "twitter":
+    source = twitter;
+    break;
+  case "weibo":
+    source = weibo;
+}
+</script>
+<template>
+  <img :src="source" @click="Window.location.assign(href)" />
+</template>
