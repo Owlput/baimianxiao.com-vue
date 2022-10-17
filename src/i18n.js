adc837c3ae48e810c8742aa57bd5b314b5666cc2
DrHuangMHT <DrHuangMHT@outlook.com>
2022-10-17 19:49:41 +0800

    better i18n intergration
    Signed-off-by: DrHuangMHT <DrHuangMHT@outlook.com>

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 import zhCn from "./assets/i18n/zh.json";
 import enUs from "./assets/i18n/en.json";
-import { createI18n } from "vue-i18n/dist/vue-i18n.esm-bundler";
+import { createI18n } from "vue-i18n";
 
 const i18n = createI18n({
   locale: "zh",
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve:{
+    alias:{
+      'vue-i18n':"vue-i18n/dist/vue-i18n.cjs.js"
+    }
+  },
   plugins: [
     vue(),
     AutoImport({ resolvers: [ElementPlusResolver()] }),
