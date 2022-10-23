8cc91b53c95bb1918c0e54e89121ffb42b149d56
DrHuangMHT <DrHuangMHT@outlook.com>
2022-10-23 10:41:30 +0800

    fix: unexpected license type fallhrough

--- src/components/LicenseType.vue
+++ src/components/LicenseType.vue
@@ -11,19 +11,12 @@ const props = defineProps({
   id: String,
 });
 
-switch (
-  props.type // Compatibility layer for old backend
-) {
-  case "CC0": {
-    part = "/zero/1.0";
-
-  }
-  case "CC BY-NC-ND 4.0": {
-    part = "/by-nc-nd/4.0";
-    break;
-  }
-  default:
-    part = "";
+// Compatibility layer for old backend
+//prettier-ignore
+switch ( props.type ) {
+  case "CC0": part = "/zero/1.0"; break;
+  case "CC BY-NC-ND 4.0": part = "/by-nc-nd/4.0"; break;
+  default: part = "";
 }
 let licenseTemplate = `<a rel="license" href="http://creativecommons.org/licenses${part}">
     <img alt="Creative Commons License - ${props.type}" src="https://i.creativecommons.org/l${part}/88x31.png" />
