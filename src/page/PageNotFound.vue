d912daaed92efb7d0f5d69116ed27ff286dd45c2
DrHuangMHT <DrHuangMHT@outlook.com>
2022-10-27 20:22:58 +0800

    make 404 page translate-able

--- src/page/PageNotFound.vue
+++ src/page/PageNotFound.vue
@@ -1,6 +1,10 @@
+<script setup>
+import {useI18n} from "vue-i18n"
+const {t} = useI18n()
+</script>
 <template>
   <main class="flex flex-col justify-center">
-    <h1>The page you want to visit does not exist.</h1>
-    <button @click="this.$router.go(-1)">Go Back</button>
+    <h1 class="text-xl m-16">{{t("notFound.page")}}</h1>
+    <button @click="this.$router.go(-1)">{{t("notFound.back")}}</button>
   </main>
 </template>
